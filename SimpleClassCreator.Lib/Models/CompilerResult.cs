9c325c3d8073d4dc67202f28339dc95acd36584c
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2022-01-06 22:40:59 -0500

    Added in dynamic C# class compiler code. Not sure if it works the way it is right now. Need to start redesigning the DTO maker screen before I can do wire up.

---
+++ SimpleClassCreator.Lib/Models/CompilerResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassCreator.Lib.Models
+{
+    public class CompilerResult
+    {
+        public bool CompiledSuccessfully { get; set; }
+
+        public string ClassName { get; set; }
+
+        public object Instance { get; set; }
+
+        public Type Type { get; set; }
+
+        public List<string> Errors { get; set; }
+    }
+}
