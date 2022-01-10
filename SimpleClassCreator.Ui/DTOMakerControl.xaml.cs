e4ae05f2d0fb8a4e29f2b0fdf4822af10cdcb333
ArchimedesJr\Eli <dyslexicanaboko@gmail.com>
2022-01-10 00:29:29 -0500

    Overhauled the treeview control's display. Instead of doing it from the code behind moved it to XAML where it belongs. Having a horrible time figuring it out and now it looks bad, but I will have to just keep working on it until it is fixed.

--- SimpleClassCreator.Ui/DTOMakerControl.xaml.cs
+++ SimpleClassCreator.Ui/DTOMakerControl.xaml.cs
@@ -86,38 +86,13 @@ Please keep in mind casing matters.";
                     _generator.GetListOfClasses() : 
                     _generator.GetClassProperties(TxtFullyQualifiedClassName.Text);
 
-                LoadTreeView(asm);
+                //LoadTreeView(asm);
+                TvAssembliesAndClasses.ItemsSource = new[] {asm};
             }
             catch (Exception ex)
             {
                 ex.ShowAsErrorMessage();
             }
-        }
-
-        private void LoadTreeView(AssemblyInfo assembly)
-        { 
-            var asm = new TreeViewItem();
-            
-            asm.Header = assembly.Name;
-            asm.IsExpanded = true;
-
-            foreach(var ci in assembly.Classes)
-            {
-                var cls = new TreeViewItem();
-                cls.Header = ci.FullName;
-                cls.IsExpanded = true;
-                cls.MouseDoubleClick += TreeViewClasses_OnMouseDoubleClick;
-
-                foreach (var pi in ci.Properties)
-                {
-                    cls.Items.Add(MakeOption(pi));
-                }
-
-                asm.Items.Add(cls);
-            }
-
-            TvAssembliesAndClasses.Items.Clear();
-            TvAssembliesAndClasses.Items.Add(asm);
         }
 
         private void TreeViewClasses_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
@@ -129,47 +104,24 @@ Please keep in mind casing matters.";
             TxtFullyQualifiedClassName.Text = Convert.ToString(tvi.Header);
         }
 
-        private StackPanel MakeOption(ClassProperty info)
-        {
-            var cbx = new CheckBox();
-            cbx.Name = "Cb" + info.Name;
-            cbx.IsChecked = true;
-
-            //TODO: Many to dos
-            /* When the row is highlighted the blue text is hard to read
-             * Make the background black
-             * Maybe have a radio button per class? That way it's clear only one can be used at a time.
-             * Show the number of properties next to the class.
-             *      For the double click event:
-             *          1. Find way to get ClassProperty object from event
-             *          2. When user double clicks class name automatically load that class
-             *              a. Or maybe dynamically load just that class?
-             *          3. Have a back button to start over?
-             * How to handle large assemblies?
-             * This should be asynchronous with a way to cancel the task
-             * Progress bar can be shown if any of this is measurable
-             * Select/Deselect all
-             * Select multiple using CTRL and SHIFT keys as normal
-             * Generate DTO button should read from the Tree View to take full advantage of it
-             * I want the colors to match Visual Studio, primitives are dark blue, and objects are a teal
-             * Extract interface option. What other options make sense? */
-            var lblType = new Label();
-            lblType.Name = "Lbl" + info.TypeName;
-            lblType.Content = info.TypeName;
-            lblType.Foreground = Brushes.CadetBlue;
-
-            var lblName = new Label();
-            lblName.Name = "Lbl" + info.Name;
-            lblName.Content = info.Name;
-
-            var sp = new StackPanel();
-            sp.Orientation = Orientation.Horizontal;
-            sp.Children.Add(cbx);
-            sp.Children.Add(lblType);
-            sp.Children.Add(lblName);
-
-            return sp;
-        }
+        //TODO: Many to dos
+        /* When the row is highlighted the blue text is hard to read
+         * Show the number of properties next to the class.
+         *      For the double click event:
+         *          1. Find way to get ClassProperty object from event
+         *          2. When user double clicks class name automatically load that class
+         *              a. Or maybe dynamically load just that class?
+         *          3. Have a back button to start over?
+         * How to handle large assemblies?
+         * This should be asynchronous with a way to cancel the task
+         * Progress bar can be shown if any of this is measurable
+         * Select/Deselect all
+         * Select multiple using CTRL and SHIFT keys as normal
+         * Generate DTO button should read from the Tree View to take full advantage of it
+         * I want the colors to match Visual Studio, primitives are dark blue, and objects are a teal
+         * Extract interface option. What other options make sense?
+         * RGB 86,156,214 - #569cd6 - visual studio keyword
+         * */
 
         private DtoMakerParameters GetParametersFromUi()
         {
